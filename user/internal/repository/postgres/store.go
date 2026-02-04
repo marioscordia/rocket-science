573546f3b0f214e144e05f63743fdbfb254c01f1
Medet <medet.akhmetov@mechta.kz>
2026-02-04 16:54:06 +0500

    user service changes

--- user/internal/repository/postgres/store.go
+++ user/internal/repository/postgres/store.go
@@ -1,4 +1,24 @@
 package postgres
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/marioscordia/rocket-science/user/internal/model"
+)
+
 type store struct {
+	pool *pgxpool.Pool
+}
+
+func (s *store) GetUserByID(ctx context.Context, userID string) (*model.User, error) {
+	// Implement the logic to get a user by ID from the database
+}
+
+func (s *store) GetUserByUsername(ctx context.Context, username string) (*model.User, error) {
+	// Implement the logic to get a user by username from the database
+}
+
+func (s *store) CreateUser(ctx context.Context, user *model.User) (string, error) {
+	// Implement the logic to create a new user in the database
 }
